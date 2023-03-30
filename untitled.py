57faf274860d08f191fbc63aea006ffbfa0db7d3
Jeremy Idogun <idogunjeremiah@gmail.com>
2023-03-30 12:04:30 +0100

    data cleaning script created

--- untitled.py
+++ untitled.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 path = '/Users/jeremyidogun/Documents/Data Science Job Market Project /chromedriver'
 
-df = gs.get_jobs('data scientist', 50, False, path, 20)
+df = gs.get_jobs('data scientist', 1000, False, path, 20)
